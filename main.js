84b88a3ee0d0193cac52cc858f701ada077f8184
Diana Vidal <diana.vidalda@gmail.com>
2023-10-23 23:10:13 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('Desarrollo sitios web.')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
+/*animacion */
+document.body.addEventListener("pointermove", (e)=>{
+  const { currentTarget: el, clientX: x, clientY: y } = e;
+  const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
+  el.style.setProperty('--posX',  x-l-w/2);
+  el.style.setProperty('--posY',  y-t-h/2);
+})
+
+
+
+/*///////////*/
